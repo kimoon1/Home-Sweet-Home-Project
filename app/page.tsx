d7d960ebf5c23d2e31ec3be7420031fa376864e9
kimoon <kimoon212@gmail.com>
2024-12-09 16:23:06 +0900

    tailwindcss 스크립트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <>
+      <h1 className="w-full text-2xl font-bold text-center">청약정보 모아보기</h1>
+      <hr className="my-4" />
+      
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,tsx}"],
+  content: ["./app/**/*.tsx"],
   theme: {
     extend: {},
   },
