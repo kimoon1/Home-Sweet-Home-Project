8c5189750b56465baf1ee11e88a1d5d212e3748d
kimoon <kimoon212@gmail.com>
2024-12-12 16:46:30 +0900

    코드 처리 시도중

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -41,7 +41,7 @@ export default async function Page() {
 
   // const {data} = await getData();
   const LHdata = await getDataLH();
-  const LHMap = LHdata[1].dsList;
+  // const LHMap = LHdata[1].dsList;
 
   // console.log(LHMap[0]);
   // console.log(data.data);
@@ -60,7 +60,7 @@ export default async function Page() {
         <div className="text-slate-900 py-2.5 border-b text-sm border-b-neutral-400 bg-indigo-50 text-center">상태</div>
 
         {/*여기부터 시작 */}
-        {LHMap.length > 0 && LHMap.map((LHitem, idx) => (
+        {LHdata[1].dsList.map((LHitem, idx) => (
           <React.Fragment key={idx}>
             <div className="py-2 text-sm flex border-b border-b-neutral-200 justify-center">{LHitem.RNUM}</div>
             <div className="py-2 text-sm flex border-b border-b-neutral-200 justify-center">{LHitem.AIS_TP_CD_NM}</div>
